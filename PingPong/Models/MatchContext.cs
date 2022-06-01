c0cf4c15f471af60cf38b308921eceaee4f57840
thomas <thomas.duditlieux@gmail.com>
2022-06-01 14:48:05 +0200

    add contexts

---
+++ PingPong/Models/MatchContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PingPong.Models
+{
+    public class MatchContext : DbContext
+    {
+        public MatchContext(DbContextOptions<MatchContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Match> Matchs { get; set; }
+    }
+}
---
+++ PingPong/Models/TournamentContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PingPong.Models
+{
+    public class TournamentContext : DbContext
+    {
+        public TournamentContext(DbContextOptions<TournamentContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Tournament> Tournaments { get; set; }
+    }
+}
