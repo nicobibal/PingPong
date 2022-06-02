ffb93e4c0e412ec167564de8a538772183fd9611
Paul Maurette <paul.maurette@gmail.com>
2022-06-02 15:58:31 +0200

    Swagger

--- PingPong/Startup.cs
+++ PingPong/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using PingPong.Models;
 using Pomelo.EntityFrameworkCore.MySql.Storage;
 using System;
@@ -30,6 +31,11 @@ namespace PingPong
                    options.UseMySql(Configuration.GetConnectionString("Default")));
 
             services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "V1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -53,6 +59,14 @@ namespace PingPong
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                
+            });
 
 
 
